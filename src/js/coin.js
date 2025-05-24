07f900d16dcdf078c7366693057b67915ab7e45b
Daniel Tran <danieltran354@gmail.com>
2025-05-24 09:08:57 +0200

    fixed collision and sprite size for coin

--- src/js/coin.js
+++ src/js/coin.js
@@ -1,17 +1,25 @@
-import { Actor, Color, DisplayMode, Engine, Loader, Vector } from "excalibur"
+import { Actor, Color, DisplayMode, Engine, Loader, Vector, CollisionType, Shape } from "excalibur"
 import { Resources } from "./resources"
 
 export class Coin extends Actor {
 
     constructor(){
+        // Get original image dimensions and scale them
+        const scale = 0.1
+        const width = Resources.Coin.width * scale
+        const height = Resources.Coin.height * scale
+        const radius = width / 2 // radius is half the width
+
         super({ 
             pos: new Vector(800,500),
-            width: 8,
-            height: 8
+            width: width,
+            height: height,
+            collider: Shape.Circle(radius)
         })
 
         // Sets Image to coin 
         const sprite = Resources.Coin.toSprite()
+        sprite.scale = new Vector(0.1, 0.1)
         this.graphics.use(sprite)
         console.log('Coin Created')
     }
